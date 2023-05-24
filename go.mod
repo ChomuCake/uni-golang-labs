440509eb0d17fa2e88f09629420cd63d5497a1fa
Illya Grenoske <uukpa56@gmail.com>
2023-05-24 17:08:12 +0300

    add mysql connection, userID in expenses, token

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/ChomuCake/uni-golang-labs
 
 go 1.19
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-sql-driver/mysql v1.7.1
+)
--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"sort"
-	"strconv"
 	"strings"
 	"text/template"
 	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type User struct {
@@ -22,6 +26,7 @@ type Expense struct {
 	Date     time.Time `json:"date"`
 	Category string    `json:"category"`
 	Amount   int       `json:"amount"`
+	UserID   int       `json:"user_id"`
 }
 
 type ByDate []Expense
@@ -31,12 +36,9 @@ func (a ByDate) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByDate) Less(i, j int) bool { return a[i].Date.Before(a[j].Date) }
 
 var (
-	tmpl              *template.Template
-	currentUserID     int
-	authUserID        int
-	currentExpensesID int
-	users             []User
-	expenses          []Expense
+	tmpl      *template.Template
+	db        *sql.DB
+	secretKey = []byte("fd9f5dc52a0b5728c5182c593e0fae7d821e6c7a0fe64b78e67450a0a6860d63")
 )
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
@@ -59,20 +61,55 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Перевірка на існування користувача з таким же ім'ям
-	for _, u := range users {
-		if u.Username == user.Username {
-			// Користувач з таким ім'ям вже існує
+	_, err = getUserByUsername(user.Username)
+	if err == nil {
+		w.Header().Set("X-Error-Message", "User with this name is already registered")
+		w.WriteHeader(http.StatusConflict) // Код 409 - Conflict, якщо користувач вже існує
+		return
+	}
+
+	err = addUser(user)
+	if err != nil {
+		if err == sql.ErrNoRows {
 			w.WriteHeader(http.StatusConflict)
 			return
 		}
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
-	currentUserID++
-	user.ID = currentUserID
-	users = append(users, user)
-
 	w.WriteHeader(http.StatusCreated)
+}
+
+func generateToken(user User) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":       user.ID,
+		"username": user.Username,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(), // Токен дійсний протягом 24 годин
+	})
+
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func verifyToken(tokenString string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); !ok || !token.Valid {
+		return nil, jwt.ErrSignatureInvalid
+	}
+
+	return token, nil
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
@@ -83,15 +120,25 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Perform user authentication
-	for _, u := range users {
-		if u.Username == user.Username && u.Password == user.Password {
-			authUserID = u.ID
-			w.WriteHeader(http.StatusOK)
+	existingUser, err := getUserByUsernameAndPassword(user.Username, user.Password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
-	w.WriteHeader(http.StatusUnauthorized)
+
+	tokenString, err := generateToken(existingUser)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Встановлення токена в заголовок відповіді
+	w.Header().Set("Authorization", tokenString)
+	w.WriteHeader(http.StatusOK)
 }
 
 func expensesHandler(w http.ResponseWriter, r *http.Request) {
@@ -103,20 +150,75 @@ func expensesHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		// Якщо користувач не авторизований
-		if authUserID == 0 {
+		// Отримання токена з заголовка авторизації
+		tokenString := strings.TrimSpace(strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer "))
+
+		// Перевірка токена
+		token, err := verifyToken(tokenString)
+		if err != nil {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
 
-		currentExpensesID++
-		expense.ID = currentExpensesID
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		userID, ok := claims["id"].(float64)
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		// Перевірка, чи користувач існує
+		existingUser, err := getUserByID(int(userID))
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
 		expense.Date = time.Now()
-		expenses = append(expenses, expense)
+		expense.UserID = existingUser.ID
+
+		err = addExpense(expense)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
 		w.WriteHeader(http.StatusCreated)
 	} else if r.Method == http.MethodGet {
-		if authUserID == 0 {
+		tokenString := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
+
+		// Перевірка токена
+		token, err := verifyToken(tokenString)
+		if err != nil {
 			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		userID, ok := claims["id"].(float64)
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		// Перевірка, чи користувач існує
+		existingUser, err := getUserByID(int(userID))
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		userExpenses, err := getUserExpenses(existingUser.ID)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
@@ -124,25 +226,35 @@ func expensesHandler(w http.ResponseWriter, r *http.Request) {
 
 		switch sortExpensesBy {
 		case "day":
-			sort.Sort(ByDate(expenses))
+			sort.Sort(ByDate(userExpenses))
 		case "month":
-			sort.SliceStable(expenses, func(i, j int) bool {
-				return expenses[i].Date.Month() < expenses[j].Date.Month()
+			sort.SliceStable(userExpenses, func(i, j int) bool {
+				return userExpenses[i].Date.Month() < userExpenses[j].Date.Month()
 			})
 		case "all":
-			sort.SliceStable(expenses, func(i, j int) bool {
-				return expenses[i].Date.Before(expenses[j].Date)
+			sort.SliceStable(userExpenses, func(i, j int) bool {
+				return userExpenses[i].Date.Before(userExpenses[j].Date)
 			})
 		}
 
 		w.Header().Set("Content-Type", "application/json")
-		err := json.NewEncoder(w).Encode(expenses)
+		err = json.NewEncoder(w).Encode(userExpenses)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 	} else if r.Method == http.MethodDelete {
-		// Parse expense ID from URL path
+		// Отримання токена з заголовка авторизації
+		tokenString := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
+
+		// Перевірка токена
+		_, err := verifyToken(tokenString)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		// Розбиття URL шляху для отримання ID витрати
 		pathParts := strings.Split(r.URL.Path, "/")
 		if len(pathParts) != 3 {
 			w.WriteHeader(http.StatusBadRequest)
@@ -150,35 +262,141 @@ func expensesHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		expenseID := pathParts[2]
 
-		// Convert expenseID to an integer
-		expenseIDInt, err := strconv.Atoi(expenseID)
+		err = deleteExpense(expenseID)
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
+			w.WriteHeader(http.StatusNotFound)
 			return
 		}
 
-		// Find and delete the expense from the expenses slice
-		for i, expense := range expenses {
-			if expense.ID == expenseIDInt {
-				expenses = append(expenses[:i], expenses[i+1:]...)
-				w.WriteHeader(http.StatusOK)
-				return
-			}
-		}
-
-		w.WriteHeader(http.StatusNotFound)
-		return
+		w.WriteHeader(http.StatusOK)
 	} else {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
+}
 
+func initDB() error {
+	var err error
+	dsn := "root:12345@tcp(localhost:3306)/test?parseTime=true"
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func closeDB() {
+	db.Close()
+}
+
+func addUser(user User) error {
+	stmt, err := db.Prepare("INSERT INTO users(username, password) VALUES(?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(user.Username, user.Password)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getUserByUsernameAndPassword(username, password string) (User, error) {
+	var user User
+	err := db.QueryRow("SELECT id, username FROM users WHERE username = ? AND password = ?", username, password).Scan(&user.ID, &user.Username)
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func getUserByUsername(username string) (User, error) {
+	var user User
+	err := db.QueryRow("SELECT id, username FROM users WHERE username = ?", username).Scan(&user.ID, &user.Username)
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func getUserByID(userID int) (User, error) {
+	// Виконання запиту до бази даних для отримання користувача за його ідентифікатором
+	query := "SELECT id, username FROM users WHERE id = ?"
+	row := db.QueryRow(query, userID)
+
+	var user User
+	err := row.Scan(&user.ID, &user.Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return User{}, fmt.Errorf("User not found")
+		}
+		return User{}, err
+	}
+
+	return user, nil
+}
+
+func getUserExpenses(userID int) ([]Expense, error) {
+	// Виконання запиту до бази даних для отримання витрат користувача за його ідентифікатором
+	query := "SELECT id, amount, category, date FROM expenses WHERE user_id = ?"
+	rows, err := db.Query(query, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var expenses []Expense
+	for rows.Next() {
+		var expense Expense
+		err := rows.Scan(&expense.ID, &expense.Amount, &expense.Category, &expense.Date)
+		if err != nil {
+			return nil, err
+		}
+		expenses = append(expenses, expense)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return expenses, nil
+}
+
+func addExpense(expense Expense) error {
+	// Виконання запиту до бази даних для збереження витрати
+	query := "INSERT INTO expenses (amount, category, date, user_id) VALUES (?, ?, ?, ?)"
+	_, err := db.Exec(query, expense.Amount, expense.Category, expense.Date, expense.UserID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func deleteExpense(expenseID string) error {
+	// Виконання запиту до бази даних для видалення витрати за її ідентифікатором
+	query := "DELETE FROM expenses WHERE id = ?"
+	_, err := db.Exec(query, expenseID)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func init() {
 	tmpl = template.Must(template.ParseFiles("pagesHTML/index.html"))
+	err := initDB()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
+	defer closeDB()
+
 	http.HandleFunc("/", mainHandler)
 	http.HandleFunc("/register", registerHandler)
 	http.HandleFunc("/login", loginHandler)
